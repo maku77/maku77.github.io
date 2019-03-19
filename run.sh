31df8a0a32d0a660c92157d5eb1ed231ee409c2a
maku77 <makkuma77@gmail.com>
2019-03-19 11:35:31 +0900

    Enable limit_post flag again

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-bundle exec jekyll serve --destination $HOME/y/jekyll_temp --incremental --host=0.0.0.0
+bundle exec jekyll serve --destination $HOME/y/jekyll_temp --incremental --limit_posts 1 --host=0.0.0.0
