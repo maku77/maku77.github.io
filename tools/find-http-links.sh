13db700f0afefb851dea757a967ce7ad29af2b66
maku77 <makkuma77@gmail.com>
2022-05-15 16:19:20 +0900

    Ignore links to localhost in find-http-links.sh

--- tools/find-http-links.sh
+++ tools/find-http-links.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-find . -name '*.html' -not -path '*/vendor/*' | xargs grep 'http:'
+find . -name '*.html' -not -path '*/vendor/*' | xargs grep 'http:' | grep -v 'http://localhost'
+find . -name '*.md'   -not -path '*/vendor/*' | xargs grep 'http:' | grep -v 'http://localhost'
 
