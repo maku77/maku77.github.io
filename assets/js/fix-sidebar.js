a4e5e9d22451bc3ffb91f00d1ebeb3f30ef8caf1
maku77 <makkuma77@gmail.com>
2024-02-06 00:42:00 +0900

    Replace all JS code using jQuery

--- assets/js/fix-sidebar.js
+++ assets/js/fix-sidebar.js
@@ -1,33 +1,32 @@
-$(function() {
-  $(window).on('resize', handleResize);
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('resize', handleResize);
   handleResize();
 
   function handleResize() {
-    var $sidebarNotFixed = $('#xContainer_sidebar_notFixed');
-    var $sidebarFixed = $('#xContainer_sidebar_fixed');
-    var $elems = $('#xContainer_sidebar').find('[data-sticky]');
-    var winHeight = $(window).height();
+    const sidebarNotFixed = document.getElementById('xContainer_sidebar_notFixed');
+    const sidebarFixed = document.getElementById('xContainer_sidebar_fixed');
+    const elems = document.getElementById('xContainer_sidebar').querySelectorAll('[data-sticky]');
+    const winHeight = window.innerHeight;
 
     // data-sticky 属性を持つ要素を後ろから見ていき、
     // ウィンドウ内に収まる要素にフラグを立てる
-    //（data-sticky 属性の値を true にする）。
+    //（data-sticky 属性の値を 'true' にする）。
     // $(...get().reverse()) は要素を逆順に処理するイディオム。
-    var sum = 0;
-    $($elems.get().reverse()).each(function() {
-      sum += $(this).outerHeight(true);
-      $(this).data('sticky', sum < winHeight);
+    let sum = 0;
+    Array.from(elems).reverse().forEach(function(elem) {
+      sum += elem.offsetHeight;
+      elem.dataset.sticky = sum < winHeight;
     });
 
     // サイドバー内の要素を #sidebar_notFixed と、
     // #sidebar-fixed の子要素として振り分ける。
-    // 順番がおかしくならないようにループを分ける。
-    $elems.each(function() {
-      if ($(this).data('sticky')) {
-        $sidebarFixed.append($(this));
+    // 順番がおかしくならないようにループを分けている。
+    elems.forEach(function(elem) {
+      if (elem.dataset.sticky === 'true') {
+        sidebarFixed.appendChild(elem);
       } else {
-        $sidebarNotFixed.append($(this));
+        sidebarNotFixed.appendChild(elem);
       }
     });
   }
 });
-
